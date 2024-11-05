eedb4bf3b3949a741c07056a83967997abab192b
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2024-11-05 14:50:17 -0400

    refac: melhoria de precisao

--- api/solar/models.py
+++ api/solar/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import (
     MinValueValidator,
     MaxValueValidator,
+    
 )
 # Create your models here.
 
@@ -9,5 +10,4 @@ class Solar(models.Model):
     numero_placas = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(180)])
     valor_placa = models.FloatField(validators=[MinValueValidator(1)])
     valor_energia = models.FloatField(validators=[MinValueValidator(1)])
-    
-    
+    temperatura = models.FloatField(null=False, blank=False, default=0.0)
